9e8e67c28402127d06c08a236d71c0f71d9a5280
TYiC <10415101@gm.fgu.edu.tw>
2022-10-28 12:28:28 +0800

    2022-10-28 修改程式與新增擴充功能

--- webpack.config.js
+++ webpack.config.js
@@ -138,7 +138,7 @@ module.exports = [
             new HtmlWebpackPlugin({
                 chunks: ['lib.min', 'gui'],
                 template: 'src/playground/index.ejs',
-                title: 'Scratch OSEP 線上編輯器',
+                title: 'OSEP_scratch',
                 sentryConfig: process.env.SENTRY_CONFIG ? '"' + process.env.SENTRY_CONFIG + '"' : null
             }),
             new HtmlWebpackPlugin({
